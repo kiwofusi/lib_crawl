d941999b761a4f6fccf23c05f2b61932f80c9cd1
kiwofusi <kiwofusi@gmail.com>
2018-09-17 20:14:58 +0900

    clis対応

---
+++ ClisClient.php
@@ -0,0 +1,87 @@
+<?php
+
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler; //インスペクション対策
+
+/**
+ * URLが「clis」となっている図書館システムのクライアント
+ *
+ * 本当に「clis」全般で動くかは未検証
+ */
+class ClisClient
+{
+    private $client;
+    private $crawler;
+
+    /**
+     * ClisClient constructor.
+     * @param $login_url string ログインURL
+     * @param $login_params array ログイン情報 [name => value]
+     * @param $form_selector string ログインフォームのセレクタ
+     */
+    public function __construct($login_url, $login_params, $form_selector = "form")
+    {
+        $this->client = new Client();
+        $this->crawler = $this->client->request('GET', $login_url);
+        $form = $this->crawler->filter($form_selector)->form();
+        $this->crawler = $this->client->submit($form, $login_params);
+    }
+
+    //public
+
+    /**
+     * 貸出状況を取得する
+     * @return array 貸出状況(No, type, title, id, place, lent_at, expiration, extend)
+     */
+    public function getBorrowing()
+    {
+        $this->clickLink("貸出状況照会へ");
+        $list = $this->crawler->filter("table.FULL tbody tr")->each(function (Crawler $tr) {
+            $item = $tr->filter("td")->each(function (Crawler $td) {
+                return $this->zenkaku_trim($td->text());
+            });
+            return array_combine([
+                "No", "type", "title", "id", "place", "lent_at", "expiration", "extend"
+            ], $item);
+        });
+        $this->back();
+        return $list;
+    }
+
+    /**
+     * 予約状況を取得する
+     * @return array 予約状況(No, type, title, place, reserved_at, status, expiration, stock, order)
+     */
+    public function getReservation()
+    {
+        $this->clickLink("予約状況照会へ");
+        $list = $this->crawler->filter("table.FULL tbody tr")->each(function (Crawler $tr) {
+            $item = $tr->filter("td")->each(function (Crawler $td) {
+                return $this->zenkaku_trim($td->text());
+            });
+            return array_combine([
+                "No", "type", "title", "place", "reserved_at", "status", "expiration", "stock", "order"
+            ], $item);
+        });
+        $this->back();
+        return $list;
+    }
+
+    //private
+
+    private function clickLink($value)
+    {
+        $link = $this->crawler->selectLink($value)->link();
+        $this->crawler = $this->client->click($link);
+    }
+
+    private function back()
+    {
+        $this->clickLink("メニューへ戻る");
+    }
+
+    private function zenkaku_trim($str)
+    {
+        return trim(preg_replace("/^[\s　]+|[\s　]+$/u", " ", $str));
+    }
+}
---
+++ crawl_clis_sample.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once 'ClisClient.php';
+
+/* 実行結果イメージ
+
+【貸出状況】(1件)
+＊＊＊＊書名＊＊＊＊ : 2018年9月24日まで : 延長不可
+
+【予約状況】(5件)
+＊＊＊＊書名＊＊＊＊＊＊＊＊＊＊＊＊ : 受取可 : 2018年9月23日まで
+＊＊＊＊書名＊＊＊＊＊＊＊＊＊＊ : 2018年9月24日まで
+＊＊＊＊書名＊＊＊＊＊＊ : 予約中 : 4位（あと56日くらい）
+＊＊＊＊書名＊＊＊＊＊＊＊＊＊＊＊＊＊＊ : 予約中 : 22位（あと154日くらい）
+＊＊＊＊書名＊＊＊＊＊＊＊＊＊＊ : 予約中 : 15位（あと210日くらい）
+
+ */
+
+//login
+$login_url = "https://libweb.city.setagaya.tokyo.jp/idcheck.shtml";
+$form_selector = "form.loginform";
+$login_params = [
+    "UID" => "********",
+    "PASS" => "********",
+];
+$client = new ClisClient($login_url, $login_params, $form_selector);
+
+//貸出状況
+$list = $client->getBorrowing();
+if (count($list) >= 1) {
+    echo "\n【貸出状況】(" . count($list) . "件)\n";
+}
+foreach ($list as $item) {
+    echo $item["title"] . " : " . $item["expiration"] . "まで : 延長" . $item["extend"] . "\n";
+}
+
+//予約状況
+$list = $client->getReservation();
+if (count($list) >= 1) {
+    echo "\n【予約状況】(" . count($list) . "件)\n";
+}
+foreach ($list as $item) {
+    echo $item["title"] . " : " . $item["status"];
+    if (strlen($item["expiration"]) > 0) {
+        echo " : " . $item["expiration"] . "まで";
+    }
+    if ((int)$item["order"] >= 1) {
+        echo " : " . $item["order"] . "位";
+
+        //受取可能までの待ち日数を予測する
+        if ((int)$item["stock"] >= 1) {
+            $period = (float)$item["order"] / (float)$item["stock"] * 14; //1冊が2週間で回ると仮定する
+            echo "（あと" . (int)$period . "日くらい）";
+        }
+    }
+    echo "\n";
+}
